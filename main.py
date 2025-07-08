d166b28db753b50a7f18a89748f8b0a1af20cb09
okcode21 <cookingwithmyrecipe@gmail.com>
2025-07-08 13:44:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
+from utils.math_ops import multiply
+from utils.beautify_ops import beautify
+
 def main():
     name = input("Enter your name: ")
-    print(greet_user(name))
+    print(greet_user(name))  # <-- define this or remove
     demo()
-
-from utils.math_ops import multiply
 
 def demo():
     print("2 x 5 =", multiply(2, 5))
 
-    
 if __name__ == "__main__":
     main()
 
-from utils import beautify_ops
 json_data = {}
 beautify(json_data)
 demo()
